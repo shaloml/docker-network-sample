8d9c5f67909403e88ab9354bc977881d252e0c42
Shalom Levi <Shalom.Levi@ewave.co.il>
2020-01-30 19:39:41 +0200

    ss

--- exercise/Migrations/20200120223240_Killing.cs
+++ exercise/Migrations/20200120223240_Killing.cs
@@ -11,6 +11,27 @@ namespace ex4.Migrations
                 table: "Samurais",
                 nullable: false,
                 defaultValue: 0);
+
+            migrationBuilder.InsertData(
+               table: "SecretIdentity",
+               columns: new[] { "Id", "RealName" },
+               values: new object[] { 1, "New York" });
+
+            migrationBuilder.InsertData(
+                table: "SecretIdentity",
+                columns: new[] { "Id", "RealName" },
+                values: new object[] { 2, "New Delhi" });
+
+
+            migrationBuilder.InsertData(
+                table: "Samurai",
+                columns: new[] { "Id", "Name", "SamuraiId", "Age", "Killing", },
+                values: new object[] { 1, "Moshe", 1, "44", 68 });
+
+            migrationBuilder.InsertData(
+                table: "Samurai",
+                columns: new[] { "Id", "Name", "SamuraiId", "Age", "Killing", },
+                values: new object[] { 2, "Omer", 2, "26", 68 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
